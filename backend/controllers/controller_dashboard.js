f8c294854020e3d0c88598a2091b3458043e755c
Aaron-bukasa <01bukasa@gmail.com>
2024-04-08 21:25:02 +0200

    mise à jour de la route accueil

--- backend/controllers/controller_dashboard.js
+++ backend/controllers/controller_dashboard.js
@@ -3,7 +3,7 @@ const express = require('express');
 exports.dashboard = (req, res) => {
     try {
         res.status(200).render('dashboard');
-        res.redirect('/')
+        // res.redirect('/')
     } catch (error) {
         console.error(error);
     }
