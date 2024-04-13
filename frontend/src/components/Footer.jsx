aa1be2c4eeca4a162b5b8f03f31cd2768f33d8ed
Aaron-bukasa <01bukasa@gmail.com>
2024-04-13 07:53:04 +0200

    restructuration et ajout du tailwind cote backend

--- frontend/src/components/Footer.jsx
+++ frontend/src/components/Footer.jsx
@@ -6,32 +6,49 @@ import imgTwitter from "../assets/images/twitter.svg";
 
 export default function Footer() {
   return (
-    <div className="max-w-screen bg-primary-blue text-white p-6 grid grid-cols-3 [max-content,max-content,max-xontent,max-content] md:grid-cols-2 md:grid-rows-[max-content,max-content,max-xontent] md:text-lg xl:grid-cols-3 xl:grid-rows-[max-content,max-content] 2xl:px-24">
-      <div className="col-start-1 col-end-4 row-start-1 row-end-2 md:col-end-2 md:mr-2">
+    <div className="max-w-screen p-6 border-t-4 grid grid-cols-5 grid-rows-[repeat(6,max-content)] md:grid-cols-2 md:grid-rows-[max-content,max-content,max-xontent] md:text-lg xl:grid-cols-3 xl:grid-rows-[max-content,max-content] 2xl:px-24">
+      <div className="col-start-1 col-end-6 row-start-1 row-end-2">
+      <img
+          src={imgLogo}
+          alt="Logo de la fédération"
+          className="w-48"
+        />
+      </div>
+      <div className="col-start-1 col-end-6 row-start-2 row-end-3 md:col-end-2 md:mr-2">
         <h2 className="font-bold text-xl mb-4 lg:text-2xl">
           Fédération des entreprises
         </h2>
-        <p className="text-gray-300">
+        <p>
           10 Av des aviateurs, Gombe,
           <br />
           Kinshasa, RDC
         </p>
-        <p className="text-gray-300">Téléphone: +243825505783</p>
-        <p className="text-gray-300">Email: contact@federation-entreprises-congo.fr</p>
-        <p className="text-gray-300">
+        <p>Téléphone: +243825505783</p>
+        <p>Email: contact@federation-entreprises-congo.fr</p>
+        <p>
           Lundi – Vendredi: 8h00 – 16h00,
           <br />
           Samedi - Dimanche: Fermé.
         </p>
       </div>
-      <form id="contact" className="col-start-1 col-end-4 row-start-2 row-end-3 flex flex-col gap-y-3 mt-6 md:col-start-2 md:col-end-3 md:row-start-1 md:row-end-2 md:mt-0 xl:mr-[10%]">
-        <h2 className="font-bold text-xl mb-4 lg:text-2xl">Contactez-nous</h2>
-        <input type="text" placeholder="Inserer votre nom" className="h-10 px-3 rounded-lg bg-gray-200 text-black placeholder:text-gray-500 outline-0 focus:bg-gray-300" />
-        <input type="email" placeholder="Inserer votre mail" className="h-10 px-3 rounded-lg bg-gray-200 text-black placeholder:text-gray-500 outline-0 focus:bg-gray-300" />
-        <textarea type="textearea" placeholder="Message" className="h-24 p-3 rounded-lg bg-gray-200 text-black placeholder:text-gray-500 outline-0 focus:bg-gray-300"></textarea>
-        <button type="button" className="w-24 py-2 mt-1 bg-sky-blue font-semibold rounded-md hover:opacity-80">Envoyer</button>
-      </form>
-      <div className="col-start-1 col-end-4 row-start-3 row-end-4 mt-12 md:col-end-1 md:row-start-2 md:row-end-3 xl:col-start-3 xl:col-end-4 xl:row-start-1 xl:row-end-2 xl:mt-0 ">
+      <ul className="col-start-1 col-end-6 row-start-3 row-end-4 flex flex-col gap-y-3 mt-6 md:col-start-2 md:col-end-3 md:row-start-1 md:row-end-2 md:mt-0 xl:mr-[10%]">
+                    <h2>Menu</h2>
+                    <li><Link to="/" className='font-semibold text-black hover:text-[#4885ff]'>Accueil</Link></li>
+                    <li><a href="/#avantages" className='font-semibold text-black hover:text-[#4885ff]'>Avantages</a></li>
+                    <li><Link to="formulaireDmd" className='font-semibold text-black hover:text-[#4885ff]'>Adhésion</Link></li>
+                    <li>
+                    <Link to="/faq" className='font-semibold text-black hover:text-[#4885ff]'>Faq</Link>
+                </li>
+                    <li><a href="/#contact" className='font-semibold text-black hover:text-[#4885ff]'>Contact</a></li>
+                    <li><Link to="login" className='font-semibold text-black hover:text-[#4885ff]'>Se connecter</Link></li>
+                    <li><Link to="signup" className='font-semibold text-black hover:text-[#4885ff]'>S'inscrire</Link></li>
+        </ul>
+      <div className="col-start-1 col-end-6 row-start-4 row-end-5">
+        <h2>Actions</h2>
+        <div><Link to="suiviDmd" className='text-primary-blue font-bold text-primary-blue border-2 border-primary-blue p-4 bg-white rounded-xl hover:bg-gray-100 text-xl'>Suivi de la demande</Link></div>
+          <div><Link to="formulaireDmd" className='text-xl font-semibold text-white font-bold bg-red-600 p-4 rounded-xl hover:opacity-80'>Adhérer maintenant</Link></div>
+      </div>
+      <div className="col-start-1 col-end-6 row-start-5 row-end-6 mt-12 md:col-end-1 md:row-start-2 md:row-end-3 xl:col-start-3 xl:col-end-4 xl:row-start-1 xl:row-end-2 xl:mt-0 ">
         <div className="newsletter">
           <h2 className="font-bold text-xl mb-4 lg:text-2xl xl:mb-7">
             Inscrivez-vous à notre newsletter
@@ -61,11 +78,8 @@ export default function Footer() {
             <img src={imgTwitter} alt="twitter" className="w-5" />
           </Link>
         </div>
-        <Link to="formulaireDmd" className="text-white font-bold bg-sky-blue inline-block p-3 rounded-xl my-3 hover:opacity-80">
-          Adhérez à la fédération
-        </Link>
       </div>
-      <div className="col-start-1 col-end-4 row-start-4 row-end-5 border-t-2 border-gray-400 pt-5 mt-3 md:col-end-3 md:row-start-3 md:row-end-4 xl:col-end-4 xl:mt-8 xl:py-10">
+      <div className="col-start-1 col-end-6 row-start-6 row-end-7 border-t-2 border-gray-400 pt-5 mt-3 md:col-end-3 md:row-start-3 md:row-end-4 xl:col-end-4 xl:mt-8 xl:py-10">
         <img
           src={imgLogo}
           alt="Logo de la fédération"
